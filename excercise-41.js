bd794ed3cf633c7bf5042aef2c289d16b8c20acb
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2022-10-30 11:51:06 -0500

    adds excersice 41

---
+++ excercise-41.js
@@ -0,0 +1,14 @@
+// Crea el Objeto
+// Crea una variable llamada pedro de tipo objeto literal con las siguientes llaves y valores:
+
+// nombre - "Pedro Perez"
+// edad - 30
+// activo - true
+// hobbies - un arreglo con los siguientes elementos: "programar", "squash", "piano".
+
+const pedro = {
+    nombre : "Pedro Perez",
+    edad : 30,
+    activo : true,
+    hobbies : ["programar", "squash", "piano"]
+}
